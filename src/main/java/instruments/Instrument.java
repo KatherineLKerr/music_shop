fd4fea33fe0a3bbcf2d49c72bcef33354e4aa141
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-02 11:26:49 +0100

    adds guitar and guitar test as empty files, adds instrument superclass

---
+++ src/main/java/instruments/Instrument.java
@@ -0,0 +1,32 @@
+package instruments;
+
+public abstract class Instrument {
+
+    protected String material;
+    protected String type;
+    protected double buyingPrice;
+    protected double sellingPrice;
+
+    public Instrument(String material, String type, double buyingPrice, double sellingPrice) {
+        this.material = material;
+        this.type = type;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+}
