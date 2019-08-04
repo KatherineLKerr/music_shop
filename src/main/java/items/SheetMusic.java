10d435578908730c112197c4bc75a3965628e81d
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-04 18:09:18 +0100

    adds sheet music with passing tests

--- src/main/java/items/SheetMusic.java
+++ src/main/java/items/SheetMusic.java
@@ -1,8 +1,44 @@
-//package items;
-//
-//import interfaces.ISell;
-//
-//public class SheetMusic implements ISell {
-//
-//
-//}
+package items;
+
+import interfaces.ISell;
+
+public class SheetMusic implements ISell {
+
+    private String title;
+    private String artist;
+    private String genre;
+    private double buyingPrice;
+    private double sellingPrice;
+
+    public SheetMusic(String title, String artist, String genre, double buyingPrice, double sellingPrice) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public double calculateMarkup() {
+        return sellingPrice - buyingPrice;
+    }
+}
--- src/test/java/SheetMusicTest.java
+++ src/test/java/SheetMusicTest.java
@@ -1,2 +1,41 @@
+import items.SheetMusic;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SheetMusicTest {
+
+    private SheetMusic sheetMusic;
+
+    @Before
+    public void setUp() {
+        sheetMusic = new SheetMusic("whatever", "i dont know", "anything", 2.00, 5.00);
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("whatever", sheetMusic.getTitle());
+    }
+
+    @Test
+    public void hasArtist() {
+        assertEquals("i dont know", sheetMusic.getArtist());
+    }
+
+    @Test
+    public void hasGenre() {
+        assertEquals("anything", sheetMusic.getGenre());
+    }
+
+    @Test
+    public void getBuyingPrice() {
+        assertEquals(2.00, sheetMusic.getBuyingPrice(),0.01);
+    }
+
+    @Test
+    public void getSellingPrice() {
+        assertEquals(5.00, sheetMusic.getSellingPrice(),0.01);
+    }
+
 }
