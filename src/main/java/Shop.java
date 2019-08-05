07373e4dc8620b0275b30e2a3c4859a700a45fdb
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-05 07:30:40 +0100

    added shop with tests passing, mvp done

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,30 @@
+import interfaces.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> stock;
+
+    public Shop() {
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public ArrayList getStock() {
+        return stock;
+    }
+    
+    public void addItem(ISell item) {
+        stock.add(item);
+    }
+    
+    public void removeItem(ISell item) {
+        for (int i = 0; i < stock.size(); i++) {
+            ISell stockItem = stock.get(i);
+            if (stockItem == item) {
+                stock.remove(i);
+                return;
+            }
+        }
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,58 @@
+import instruments.Guitar;
+import interfaces.ISell;
+import items.ReplacementPart;
+import items.Tuner;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    private Shop shop;
+    private Guitar guitar;
+    private Tuner tuner;
+    private ReplacementPart replacementPart;
+
+    @Before
+    public void setUp() {
+        shop = new Shop();
+        guitar = new Guitar("wood", "acoustic", "wood", 300.00, 400.00, 6);
+        tuner = new Tuner("guitar", "electric", 12.00, 5.00);
+        replacementPart = new ReplacementPart("guitar", "strings", 20.00, 10.00);
+    }
+
+    @Test
+    public void canAddItem() {
+        shop.addItem(tuner);
+        assertEquals(1, shop.getStock().size());
+    }
+
+    @Test
+    public void canRemoveItem() {
+        shop.addItem(tuner);
+        shop.addItem(guitar);
+        shop.addItem(replacementPart);
+        shop.removeItem(guitar);
+        assertEquals(2, shop.getStock().size());
+    }
+
+    @Test
+    public void onlyRemovesOneItemIfDuplicates() {
+        shop.addItem(tuner);
+        shop.addItem(guitar);
+        shop.addItem(tuner);
+        shop.removeItem(tuner);
+        assertEquals(2, shop.getStock().size());
+    }
+
+    @Test
+    public void cannotRemoveIfNotInStock() {
+        shop.addItem(tuner);
+        shop.addItem(replacementPart);
+        shop.removeItem(guitar);
+        assertEquals(2, shop.getStock().size());
+    }
 }
